cf8910b20e0d8d15a32cf9863774d2a35f420957
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-10 14:38:58 +0430

    add modal's doc page

---
+++ src/app/docs/modal/page.tsx
@@ -0,0 +1,17 @@
+import Button from "@/components/Button/Button";
+import Modal from "@/components/Modal/Modal";
+import { useState } from "react";
+
+export default function Page() {
+	const [open, setOpen] = useState(false);
+	return (
+		<>
+			<div>
+				<Button>Open Modal</Button>
+				<Modal open={open} onChange={setOpen}>
+					<div>this is the Modal content</div>
+				</Modal>
+			</div>
+		</>
+	);
+}
