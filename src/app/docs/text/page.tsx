dc40e3c2fc4d8f6e67afe6d2f47aed42954f0a73
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-10 14:49:19 +0430

    add text docs page

---
+++ src/app/docs/text/page.tsx
@@ -0,0 +1,12 @@
+import Text from "@/components/Text/Text";
+
+export default function Page() {
+	return (
+		<>
+			<div>
+				<Text type='h1'>heading</Text>
+				<Text type='p'>paragraph</Text>
+			</div>
+		</>
+	);
+}
