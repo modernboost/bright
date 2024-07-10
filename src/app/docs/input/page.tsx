5f33712a5166949f125dc0b7f2fa362aab3c981c
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-10 14:33:01 +0430

    add input docs page

---
+++ src/app/docs/input/page.tsx
@@ -0,0 +1,16 @@
+import Button from "@/components/Button/Button";
+import Dropdown from "@/components/Dropdown/Dropdown";
+import DropdownItem from "@/components/Dropdown/DropdownItem";
+import Input from "@/components/Input/Input";
+import Offcanvas from "@/components/Offcanvas/Offcanvas";
+import Popup from "@/components/Popup/Popup";
+
+export default function Page() {
+	return (
+		<>
+			<div>
+				<Input />
+			</div>
+		</>
+	);
+}
