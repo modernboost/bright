f6bd2e1fc428cd7b6d2eecdb0e4d2f1bee18a491
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-29 11:07:21 +0430

    export player and gitignore

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,4 @@ export {default as Input} from './Input/Input';
 export {default as Popup} from './Popup/Popup';
 export {default as Spinner} from './Spinner/Spinner';
 export {default as Text} from './Text/Text';
+export {default as Player} from './Player/Player';
