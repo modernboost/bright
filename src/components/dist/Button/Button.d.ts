a17f4181f0384e8925fc43dd0ab20178879a9057
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-29 11:03:09 +0430

    remove the build files

--- src/components/dist/Button/Button.d.ts
+++
@@ -1,4 +0,0 @@
-export default function Button({ children, onClick, ...restProps }: {
-    onClick?: Function;
-    children?: React.ReactNode;
-}): import("react/jsx-runtime").JSX.Element;
--- src/components/dist/Card/Card.d.ts
+++
@@ -1,1 +0,0 @@
-export default function Card({ children, ...restProps }: React.ComponentProps<"div">): import("react/jsx-runtime").JSX.Element;
