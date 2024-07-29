311001f7228a4c70d18b20726915117b07ed54ea
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-29 11:01:04 +0430

    documenet the player

--- src/app/docs/button/page.tsx
+++ src/app/docs/button/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import { Button } from "@mbtjs/bright";
 
 export default function Page() {
 	return (
 		<>
 			<div>
-				<Button></Button>
+				<Button >hellow</Button>
 			</div>
 		</>
 	);
