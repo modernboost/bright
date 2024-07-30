200a08a2fa9c86d0d39ffe64432c5d8747ae2693
Jawad Azizi <jawad.azizi1398@gmail.com>
2024-07-30 14:21:33 +0430

    upldate package

--- src/components/vite.config.ts
+++ src/components/vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
 import path from "path";
+import css from 'rollup-plugin-css-only';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+import postcss from 'rollup-plugin-postcss';
 
 export default defineConfig({
+  css: {
+    modules: {
+      scopeBehaviour: 'local',
+    },
+  },
   build: {
     lib: {
       entry: path.resolve(__dirname, "index.ts"),
@@ -22,5 +31,8 @@ export default defineConfig({
     sourcemap: true,
     emptyOutDir: true,
   },
-  plugins: [react(), dts()],
+  plugins: [react(), dts(),css(),postcss({
+    extract: true,
+    plugins: [tailwindcss, autoprefixer],
+  }),],
 });
