1e2e2c9feff500f66cd9c0a60b37a0292f050637
Jawad Azizi <102347277+JawadAzizi@users.noreply.github.com>
2024-10-29 22:06:22 +0430

    add base types

---
+++ src/components/BaseTypes.ts
@@ -0,0 +1,22 @@
+export type BaseColor =
+	| "primary"
+	| "primary-content"
+	| "secondary"
+	| "secondary-content"
+	| "info"
+	| "info-content"
+	| "success"
+	| "success-content"
+	| "accent"
+	| "accent-content"
+	| "neutral-content"
+	| "warning"
+	| "warning-content"
+	| "error"
+	| "error-content"
+	| "base-100"
+	| "base-200"
+	| "base-300"
+	| "base-content";
+
+export type BaseSize = "sm" | "md" | "lg";
