9c458ae7621cfb7b13b2674f3784443215c8c33d
Jawad Azizi <102347277+JawadAzizi@users.noreply.github.com>
2024-10-29 23:15:12 +0430

    update card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,24 @@
-import styles from './Card.module.css'
-export default function Card({children, ...restProps}: React.ComponentProps<"div">){
-    return (
-        <div {...restProps} className={styles.card + restProps.className}>{children}</div>
-    )
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { BaseColor, BaseSize } from "../BaseTypes";
+type CardVariants = "title" | "image";
+export default function Card({
+	children,
+	variant,
+	className,
+	...restProps
+}: {
+	children?: React.ReactNode;
+	className?: string;
+	variant?: CardVariants;
+}) {
+
+	const classNames = clsx("card", className);
+
+	return (
+		<div className={classNames} {...restProps}>
+			{children}
+		</div>
+	);
 }
